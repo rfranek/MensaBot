7be073bddc93f7f8824d2bbac76b30910247fdd7
Gerd Schmidt <gerd@acagamics.de>
2017-01-25 14:23:49 +0100

    Added simple page parsing

---
+++ MensaBotParsing/Mensa/FoodElement.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MensaBotParsing
+{
+    class FoodElement
+    {
+        public String GermanName { get; private set; }
+        public String EnglishName { get; private set; }
+        public String Price { get; private set; }
+        public List<FoodTags> Tags { get; private set; }
+        public String AdditivesAndAllergenics { get; private set; }
+
+        public FoodElement(String germanName, String englishName, String price, List<FoodTags> tags, String additivesAndAllergenics)
+        {
+            this.GermanName = germanName;
+            this.EnglishName = englishName;
+            this.Price = price;
+            this.Tags = tags;
+            this.AdditivesAndAllergenics = additivesAndAllergenics;
+        }
+
+    }
+
+    public enum FoodTags
+    {
+        VITAL,
+        VEGAN,
+        GARLIC,
+        ALCOHOL,
+        PORK,
+        VEGETARIAN,
+        SOUP,
+        FISH,
+        CHICKEN,
+        BEEF,
+        VENSION,
+        BIO,
+        HOGGET
+
+    }
+}
